1c7a98d5ef056420a2b1b643bb41134a2844cdfa
tonyruan2 <tonyr2@illinois.edu>
2019-12-25 23:48:02 -0600

    Add dynamic graph generation and coloring

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-
-const colors = ["#F0F8FF", "#FAEBD7", "#00FFFF", "#7FFFD4", "#F0FFFF", "#F5F5DC", "#FFE4C4", "#FFEBCD", "#0000FF", "#8A2BE2", "#A52A2A", "#DEB887", "#5F9EA0", "#7FFF00", "#D2691E", "#FF7F50", "#6495ED", "#FFF8DC", "#DC143C", "#00FFFF", "#00008B", "#008B8B", "#B8860B", "#A9A9A9", "#A9A9A9", "#006400", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00", "#9932CC", "#8B0000", "#E9967A", "#8FBC8F", "#483D8B", "#2F4F4F", "#2F4F4F", "#00CED1", "#9400D3", "#FF1493", "#00BFFF", "#696969", "#696969", "#1E90FF", "#B22222", "#FFFAF0", "#228B22", "#FF00FF", "#DCDCDC", "#F8F8FF", "#FFD700", "#DAA520", "#808080", "#808080", "#008000", "#ADFF2F", "#F0FFF0", "#FF69B4", "#CD5C5C", "#4B0082", "#FFFFF0", "#F0E68C", "#E6E6FA", "#FFF0F5", "#7CFC00", "#FFFACD", "#ADD8E6", "#F08080", "#E0FFFF", "#FAFAD2", "#D3D3D3", "#D3D3D3", "#90EE90", "#FFB6C1", "#FFA07A", "#20B2AA", "#87CEFA", "#778899", "#778899", "#B0C4DE", "#FFFFE0", "#00FF00", "#32CD32", "#FAF0E6", "#FF00FF", "#800000", "#66CDAA", "#0000CD", "#BA55D3", "#9370DB", "#3CB371", "#7B68EE", "#00FA9A", "#48D1CC", "#C71585", "#191970", "#F5FFFA", "#FFE4E1", "#FFE4B5", "#FFDEAD", "#000080", "#FDF5E6", "#808000", "#6B8E23", "#FFA500", "#FF4500", "#DA70D6", "#EEE8AA", "#98FB98", "#AFEEEE", "#DB7093", "#FFEFD5", "#FFDAB9", "#CD853F", "#FFC0CB", "#DDA0DD", "#B0E0E6", "#800080", "#663399", "#FF0000", "#BC8F8F", "#4169E1", "#8B4513", "#FA8072", "#F4A460", "#2E8B57", "#FFF5EE", "#A0522D", "#C0C0C0", "#87CEEB", "#6A5ACD", "#708090", "#708090", "#FFFAFA", "#00FF7F", "#4682B4", "#D2B48C", "#008080", "#D8BFD8", "#FF6347", "#40E0D0", "#EE82EE", "#F5DEB3", "#FFFF00", "#9ACD32"];
-
-const canvas = document.getElementById("graphCanvas");
-const context = canvas.getContext("2d");
-const canvasWidth = 600;
-const canvasHeight = 600;
-const margin = 50;
-let isBaseGraphWheel = false;
-let coloringType = "uncolored"; //figure this out to work with addEdgeToGraph, addVertexToGraph
+/*
+ * Graph class that keeps track of the coloring of its vertices and edges.
+ */
 
 class Graph {
   constructor(vertexCount = 1) {
@@ -84,7 +77,8 @@ class Graph {
     }
   }
 
-  assignVertexColors() {
+  assignGreedyColoring() {
+    let used_colors = new Set();
     for (let key of this.vertexColorMap.keys()) {
       for (let color of colors) {
         let available = true;
@@ -98,11 +92,12 @@ class Graph {
         }
         if (available) {
           this.vertexColorMap.set(key, color);
-          console.log(color);
+          used_colors.add(color);
           break;
         }
       }
     }
+    console.log("Color count: " + used_colors.size);
   }
 
   assignEdgeInducedColoring() {
@@ -110,7 +105,26 @@ class Graph {
   }
 }
 
+/*
+ * Global variables used in drawing and displaying the graph.
+ */
+
+// const colors = ["red", "blue", "orange", "yellow", "green", "indigo", "violet"];
+const colors = ["#F0F8FF", "#FAEBD7", "#00FFFF", "#7FFFD4", "#F0FFFF", "#F5F5DC", "#FFE4C4", "#FFEBCD", "#0000FF", "#8A2BE2", "#A52A2A", "#DEB887", "#5F9EA0", "#7FFF00", "#D2691E", "#FF7F50", "#6495ED", "#FFF8DC", "#DC143C", "#00FFFF", "#00008B", "#008B8B", "#B8860B", "#A9A9A9", "#A9A9A9", "#006400", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00", "#9932CC", "#8B0000", "#E9967A", "#8FBC8F", "#483D8B", "#2F4F4F", "#2F4F4F", "#00CED1", "#9400D3", "#FF1493", "#00BFFF", "#696969", "#696969", "#1E90FF", "#B22222", "#FFFAF0", "#228B22", "#FF00FF", "#DCDCDC", "#F8F8FF", "#FFD700", "#DAA520", "#808080", "#808080", "#008000", "#ADFF2F", "#F0FFF0", "#FF69B4", "#CD5C5C", "#4B0082", "#FFFFF0", "#F0E68C", "#E6E6FA", "#FFF0F5", "#7CFC00", "#FFFACD", "#ADD8E6", "#F08080", "#E0FFFF", "#FAFAD2", "#D3D3D3", "#D3D3D3", "#90EE90", "#FFB6C1", "#FFA07A", "#20B2AA", "#87CEFA", "#778899", "#778899", "#B0C4DE", "#FFFFE0", "#00FF00", "#32CD32", "#FAF0E6", "#FF00FF", "#800000", "#66CDAA", "#0000CD", "#BA55D3", "#9370DB", "#3CB371", "#7B68EE", "#00FA9A", "#48D1CC", "#C71585", "#191970", "#F5FFFA", "#FFE4E1", "#FFE4B5", "#FFDEAD", "#000080", "#FDF5E6", "#808000", "#6B8E23", "#FFA500", "#FF4500", "#DA70D6", "#EEE8AA", "#98FB98", "#AFEEEE", "#DB7093", "#FFEFD5", "#FFDAB9", "#CD853F", "#FFC0CB", "#DDA0DD", "#B0E0E6", "#800080", "#663399", "#FF0000", "#BC8F8F", "#4169E1", "#8B4513", "#FA8072", "#F4A460", "#2E8B57", "#FFF5EE", "#A0522D", "#C0C0C0", "#87CEEB", "#6A5ACD", "#708090", "#708090", "#FFFAFA", "#00FF7F", "#4682B4", "#D2B48C", "#008080", "#D8BFD8", "#FF6347", "#40E0D0", "#EE82EE", "#F5DEB3", "#FFFF00", "#9ACD32"];
+
+const canvas = document.getElementById("graphCanvas");
+const context = canvas.getContext("2d");
+const canvasWidth = 600;
+const canvasHeight = 600;
+const margin = 50;
+let isBaseGraphWheel = false;
+let coloringType = "uncolored"; //figure this out to work with addEdgeToGraph, addVertexToGraph
 let g = new Graph();
+
+
+/*
+ * Functions used to draw the graph.
+ */
 
 function dividePoints(numPoints, width, height, margin, wheel = isBaseGraphWheel) {
   if (numPoints == 0) {
@@ -166,6 +180,7 @@ function drawEdge(beginX, beginY, endX, endY, color="#000000") {
 }
 
 function drawGraph(graph = g, wheel = isBaseGraphWheel) {
+  applyColoringOption();
   let pointPositions = dividePoints(graph.vertexCount, canvasWidth, canvasHeight, margin, wheel);
 
   for (let key of graph.edgeColorMap.keys()) {
@@ -187,6 +202,87 @@ function drawGraph(graph = g, wheel = isBaseGraphWheel) {
 function clearCanvas() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+/*
+ * Functions used to color the graph.
+ */
+
+function colorGraphUncolored_action() {
+  for (let key of g.vertexColorMap.keys()) {
+    g.vertexColorMap.set(key, "#FFFFFF");
+  }
+  for (let key of g.edgeColorMap.keys()) {
+    g.edgeColorMap.set(key, "#000000");
+  }
+}
+
+function colorGraphUncolored() {
+  clearCanvas();
+  colorGraphUncolored_action();
+  drawGraph();
+}
+
+function colorGraphEdgeInduced_action() {
+  for (let key of g.vertexColorMap.keys()) {
+    g.vertexColorMap.set(key, "blue");
+  }
+  for (let key of g.edgeColorMap.keys()) {
+    g.edgeColorMap.set(key, colors[Math.floor(Math.random() * colors.length)]);
+  }
+}
+
+function colorGraphEdgeInduced() {
+  clearCanvas();
+  colorGraphEdgeInduced_action();
+  drawGraph();
+}
+
+function colorGraphGreedy_action() {
+  for (let key of g.vertexColorMap.keys()) {
+    g.vertexColorMap.set(key, "#FFFFFF");
+  }
+  for (let key of g.edgeColorMap.keys()) {
+    g.edgeColorMap.set(key, "#000000");
+  }
+  g.assignGreedyColoring();
+}
+
+function colorGraphGreedy() {
+  clearCanvas();
+  colorGraphGreedy_action();
+  drawGraph();
+}
+
+function colorGraphMinimum_action() {
+  console.log("Minimum!");
+}
+
+function colorGraphMinimum() {
+  clearCanvas();
+  colorGraphGreedy_action();
+  drawGraph();
+}
+
+function applyColoringOption() {
+  let activeButtons = document.getElementById("colorOptions").getElementsByClassName("active");
+  let id = activeButtons[0].id;
+  if (id == "colorUncoloredButton") {
+    colorGraphUncolored_action();
+  }
+  else if (id == "colorEdgeInducedButton") {
+    colorGraphEdgeInduced_action();
+  }
+  else if (id == "colorGreedyButton") {
+    colorGraphGreedy_action();
+  }
+  else if (id == "colorMinimumButton") {
+    colorGraphMinimum_action();
+  }
+}
+
+/*
+ * Functions used to add to the graph.
+ */
 
 function addVertexToGraph() {
   if (g.vertexCount < 100) {
@@ -215,6 +311,28 @@ function addEdgeToGraph() {
   }
 }
 
+
+/*
+ * Functons that change the graph's display.
+ */
+
+function selectCycleDisplay() {
+  isBaseGraphWheel = false;
+  clearCanvas();
+  drawGraph();
+}
+
+function selectWheelDisplay() {
+  isBaseGraphWheel = true;
+  clearCanvas();
+  drawGraph();
+}
+
+
+/*
+ * Functions used to generate different classes of graphs.
+ */
+
 function shuffle(arr) {
   for (let i = arr.length - 1; i > 0; --i) {
     let rand_prev_index = Math.floor(Math.random() * i);
@@ -235,18 +353,6 @@ function retrieveVertexCount() {
   }
   document.getElementById("vertices").value = vertexCount.toString();
   return vertexCount;
-}
-
-function selectCycleDisplay() {
-  isBaseGraphWheel = false;
-  clearCanvas();
-  drawGraph();
-}
-
-function selectWheelDisplay() {
-  isBaseGraphWheel = true;
-  clearCanvas();
-  drawGraph();
 }
 
 function generateRandomGraph(label = "") {
@@ -396,41 +502,71 @@ function generateCompleteGraph() {
   drawGraph();
 }
 
-function colorGraphUncolored() {
-  clearCanvas();
-  for (let key of g.vertexColorMap.keys()) {
-    g.vertexColorMap.set(key, "#FFFFFF");
+/*
+ * Function to add active class to in-use buttons.
+ */
+
+function addActiveFunctionality(optionsId) {
+  let buttons = document.getElementById(optionsId).getElementsByClassName("input");
+  for (let i = 0; i < buttons.length; ++i) {
+    buttons[i].addEventListener("click", function() {
+      let current = document.getElementById(optionsId).getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    })
   }
-  for (let key of g.edgeColorMap.keys()) {
-    g.edgeColorMap.set(key, "#000000");
-  }
-  drawGraph();
 }
 
-function colorGraphEdgeInduced() {
-  clearCanvas();
-  for (let key of g.vertexColorMap.keys()) {
-    g.vertexColorMap.set(key, "blue");
+/*
+ * Functions used to change the vertex count of the displayed graph.
+ */
+
+function increaseVertexCount() {
+  let vertexCount = retrieveVertexCount();
+  if (vertexCount < 100) {
+    ++vertexCount;
+    document.getElementById("vertices").value = vertexCount.toString();
+    document.getElementById("vertices").dispatchEvent(new Event("change"));
   }
-  for (let key of g.edgeColorMap.keys()) {
-    g.edgeColorMap.set(key, colors[Math.floor(Math.random() * colors.length)]);
-  }
-  drawGraph();
 }
 
-function colorGraphVertices() {
-  clearCanvas();
-  for (let key of g.edgeColorMap.keys()) {
-    g.edgeColorMap.set(key, "black");
-  }
-  g.assignVertexColors();
-  drawGraph();
+function sameVertexCount() {
+  document.getElementById("vertices").dispatchEvent(new Event("change"));
 }
+
+function decreaseVertexCount() {
+  let vertexCount = retrieveVertexCount();
+  if (vertexCount > 1) {
+    --vertexCount;
+    document.getElementById("vertices").value = vertexCount.toString();
+    document.getElementById("vertices").dispatchEvent(new Event("change"));
+  }
+}
+
+/*
+ * Function to randomize onload graph and also add functionality to buttons.
+ */
 
 window.onload = function() {
   const vertexCount = Math.floor(Math.random() * 8) + 3;
   document.getElementById("vertices").value = vertexCount.toString();
   generateRandomGraph();
+
+  addActiveFunctionality("generateOptions");
+  addActiveFunctionality("displayOptions");
+  addActiveFunctionality("colorOptions");
+
+  document.getElementById("vertices").addEventListener("change", function() {
+    let activeButtons = document.getElementById("userInput").getElementsByClassName("active");
+    for (let i = 0; i < activeButtons.length; ++i) {
+      console.log(activeButtons[i].id);
+      document.getElementById(activeButtons[i].id).click();
+    }
+  })
+
+  document.getElementById("increaseVertexCountButton").addEventListener("click", increaseVertexCount);
+  document.getElementById("decreaseVertexCountButton").addEventListener("click", decreaseVertexCount);
+  document.getElementById("sameVertexCountButton").addEventListener("click", sameVertexCount);
 
   document.getElementById("generateRandomButton").addEventListener("click", generateRandomGraph);
   document.getElementById("generateSparseButton").addEventListener("click", generateSparseGraph);
@@ -452,5 +588,7 @@ window.onload = function() {
 
   document.getElementById("colorUncoloredButton").addEventListener("click", colorGraphUncolored);
   document.getElementById("colorEdgeInducedButton").addEventListener("click", colorGraphEdgeInduced);
-  document.getElementById("colorVerticesButton").addEventListener("click", colorGraphVertices);
+
+  document.getElementById("colorGreedyButton").addEventListener("click", colorGraphGreedy);
+  document.getElementById("colorMinimumButton").addEventListener("click", colorGraphMinimum);
 }
